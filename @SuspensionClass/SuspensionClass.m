f0a60979214916117dd8d437d154c2e5e8d592eb
Pedro Montanari <67879177+Pedro-Montanari@users.noreply.github.com>
2022-04-26 20:44:34 +0200

    Create SuspensionClass.m
    [MOD] Created SuspensionClass

---
+++ @SuspensionClass/SuspensionClass.m
@@ -0,0 +1,33 @@
+classdef SuspensionClass
+    %UNTITLED3 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        Property1
+        
+    end
+    
+    properties (SetAccess = private, Hidden = true)
+        Property2
+    end
+    
+    methods
+        %% Default constructor
+        function obj = SuspensionClass()
+        end
+        
+        %%
+        function obj = untitled3(inputArg1,inputArg2)
+            %UNTITLED3 Construct an instance of this class
+            %   Detailed explanation goes here
+            obj.Property1 = inputArg1 + inputArg2;
+        end
+        
+        function outputArg = method1(obj,inputArg)
+            %METHOD1 Summary of this method goes here
+            %   Detailed explanation goes here
+            outputArg = obj.Property1 + inputArg;
+        end
+    end
+end
+
